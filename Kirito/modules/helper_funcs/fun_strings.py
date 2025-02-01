16d288b166dfb043a23556108a2d52b80d082991
Canskuy <92519755+Canskuy@users.noreply.github.com>
2025-02-01 10:32:23 +0700

    Add files via upload

---
+++ Kirito/modules/helper_funcs/fun_strings.py
@@ -0,0 +1,17 @@
+AFK = (
+    "Papai 👋 {}",
+    "Byebye {}.",
+    "See you bitch {}",
+    "Goodbye {}",
+    "Come back soon {}",
+    "au revoir {}",
+    "Talk to you later {}",
+    "I’m off",
+    "Take care {}",
+    "Catch you later {}",
+    "Hasta la Vista {}",
+    "Despedida 👋 {}",
+    "Adios {}",
+    "Até Logo {}",
+   
+)
---
+++ Kirito/modules/helper_funcs/telethn/__init__.py
@@ -0,0 +1,8 @@
+from KiritoRobot import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES, telethn
+from KiritoRobot.telethn import *
+
+IMMUNE_USERS = DRAGONS + WOLVES + DEMONS + TIGERS + DEV_USERS
+
+IMMUNE_USERS = (
+    list(DRAGONS) + list(WOLVES) + list(DEMONS) + list(TIGERS) + list(DEV_USERS)
+)
